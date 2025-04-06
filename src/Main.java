429640f09783362fd00ca37d39011c57f55720ef
rod <dev_rodts@outlook.com>
2025-04-06 09:31:18 -0300

    chore: init project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // PacmanGame game = new PacmanGame();
+        
+        // Start the game
+        // game.startGame();
+    }
+    
+}
